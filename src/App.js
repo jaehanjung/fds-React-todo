de81c1c371df15ab37ec2c94263c0de7279b4f20
jaehanjung <wjdwogks23@gmail.com>
2018-06-07 15:57:23 +0900

    할일 추가 기능

--- src/App.js
+++ src/App.js
@@ -1,28 +1,59 @@
 import React, { Component } from "react";
 
+let count = 0;
 
 class App extends Component {
   // 클래스필드 사용 변경이될코드는 state에 넣어준다.
   state = {
     todos: [
       {
-        id: 1,
+        id: count++,
         body: "react 공부",
         complete: true
       },
       {
-        id: 2,
+        id: count++,
         body: "redux 공부",
         complete: false
-      }
-    ]
+      },
+    ],
+    newTodoBody:''
   };
+
+  // 이벤트 리스너로 사용될 메소드는 handl 을 사용하는게 관례
+  handleInputChange = e => {
+    this.setState({
+      newTodoBody: e.target.value
+    });
+  }
+
+  handleButtonClick = e =>{
+    if(this.state.newTodoBody) {
+      const newTodo = {
+        body: this.state.newTodoBody,
+        complete: false,
+        id: count++
+      }
+      this.setState({
+        todos: [
+          ...this.state.todos,
+          newTodo
+        ],
+        newTodoBody:''
+      });
+    }
+  }
   render() {
     // 클래스필드를 사용하였으니 this.state에 넣어준다.
-    const {todos} = this.state;
+    const { todos,newTodoBody } = this.state;
     return (
       <div>
         <h1>할 일 목록</h1>
+        <label>
+          새로운 할일
+          <input type="text" value={newTodoBody} onChange={this.handleInputChange}/>
+          <button onClick={this.handleButtonClick} >추가</button>
+        </label>
         <ul>
           {todos.map(todo => (
             <li className={todo.complete ? "complete" : ""} key={todo.id}>
