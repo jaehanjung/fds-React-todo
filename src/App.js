384b34669b4b8a209ac9118c8934f4306adc7348
jaehanjung <wjdwogks23@gmail.com>
2018-06-07 15:38:37 +0900

    todos 를 state로 이전

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-const todos = [
-  {
-    id:1,
-    body: 'react 공부',
-    complete: true
-  },
-  {
-    id:2,
-    body: 'redux 공부',
-    complete: false
-  }
-]
 
 class App extends Component {
+  // 클래스필드 사용 변경이될코드는 state에 넣어준다.
+  state = {
+    todos: [
+      {
+        id: 1,
+        body: "react 공부",
+        complete: true
+      },
+      {
+        id: 2,
+        body: "redux 공부",
+        complete: false
+      }
+    ]
+  };
   render() {
+    // 클래스필드를 사용하였으니 this.state에 넣어준다.
+    const {todos} = this.state;
     return (
-    <div>
-      <h1>할 일 목록</h1>
-      <ul>
-        {
-          todos.map(todo => (
-            <li key={todo.id}>{todo.body}</li>
-          ))
-        }
-      </ul>
-    </div>
+      <div>
+        <h1>할 일 목록</h1>
+        <ul>
+          {todos.map(todo => (
+            <li className={todo.complete ? "complete" : ""} key={todo.id}>
+              {todo.body}
+            </li>
+          ))}
+        </ul>
+      </div>
     );
   }
 }
