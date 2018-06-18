76e6b24666fc59e06f1e7723175b42ba494a960e
jaehanjung <wjdwogks23@gmail.com>
2018-06-18 12:52:04 +0900

    form 사용하여 로그인 폼 작성

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -3,30 +3,32 @@ import React, { Component } from "react";
 import todoAPI from "../todoAPI";
 
 export default class LoginForm extends Component {
-  usernameRef = React.createRef();
-  passwordRef = React.createRef();
-
-  handleLoginClick = async e => {
-    //로그인 요청
+ 
+  handleSubmit = e => {
+    e.preventDefault();
+    const username = e.target.elements.username.value;
+    const password = e.target.elements.password.value;
     const { onLogin } = this.props;
-    onLogin(this.usernameRef.current.value, this.passwordRef.current.value);
+    onLogin(username, password);
   };
 
   render() {
-    const { onLogin } = this.props;
     return (
-      <div>
-        <h1>로그인페이지</h1>
-        <label>
-          아이디:
-          <input type="text" ref={this.usernameRef} />
-        </label>
-        <label>
-          비밀번호:
-          <input type="password" ref={this.passwordRef} />
-        </label>
-        <button onClick={this.handleLoginClick}>login</button>
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          <h1>로그인페이지</h1>
+          <input type="checkbox" defaultChecked={true}/>
+          <label>
+            아이디:
+            <input defaultValue="fds" name="username" type="text" />
+          </label>
+          <label>
+            비밀번호:
+            <input name="password" type="password" />
+          </label>
+          <button>login</button>
+        </div>
+      </form>
     );
   }
 }
