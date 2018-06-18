a769cbbe8566a1cba9cbc9f81cab3832941be671
jaehanjung <wjdwogks23@gmail.com>
2018-06-18 12:19:41 +0900

    Ref사용하여 로그인폼 변경

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,52 +2,28 @@ import React, { Component } from "react";
 
 import todoAPI from "../todoAPI";
 
-
 export default class LoginForm extends Component {
-  state = {
-    username: "",
-    password: ""
-  };
-
-  handleUserNameChange = e => {
-    this.setState({
-      username: e.target.value
-    });
-  };
-
-  handlePasswordChange = e => {
-    this.setState({
-      password: e.target.value
-    });
-  };
+  usernameRef = React.createRef();
+  passwordRef = React.createRef();
 
   handleLoginClick = async e => {
     //로그인 요청
-    const {onLogin} = this.props;
-    onLogin(this.state.username, this.state.password);
+    const { onLogin } = this.props;
+    onLogin(this.usernameRef.current.value, this.passwordRef.current.value);
   };
 
   render() {
     const { onLogin } = this.props;
-    const { username, password } = this.state;
     return (
       <div>
         <h1>로그인페이지</h1>
         <label>
           아이디:
-          <input
-            type="text"
-            value={username}
-            onChange={this.handleUserNameChange}
-          />
+          <input type="text" ref={this.usernameRef} />
         </label>
         <label>
           비밀번호:
-          <input
-            type="password"
-            value={password}
-            onChange={this.handlePasswordChange}
-          />
+          <input type="password" ref={this.passwordRef} />
         </label>
         <button onClick={this.handleLoginClick}>login</button>
       </div>
